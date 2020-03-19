205420bf5836f6bbe7ea0a180723a8ab116b34e4
Mauro de Carvalho <mauro.dcg@gmail.com>
2020-03-19 00:38:04 -0300

    Fundamentos - Aula 1

---
+++ 1-fundamentos/anotacoes.md
@@ -0,0 +1,12 @@
+# GOROOT & GOPATH
+
+Para exibir as variáveis de ambiente da instalação do Go:
+
+```shell
+$ go env
+```
+
+```
+$ go env GOROOT # Local onde estão localizado os binários para funcionamento do Go
+$ go env GOPATH # Workspace padrão para o desenvolvimento.
+```
---
+++ 1-fundamentos/primeiro/primeiro.go
@@ -0,0 +1,17 @@
+// Programas executáveis pelo pacote main
+package main
+
+import "fmt"
+
+/*
+Os códigos em Go são organizados em pacotes
+e para usá-los é necessário declarar um ou vários
+imports
+*/
+
+// A função main é a porta de entrada de um programa
+// escrito em Go.
+func main() {
+	fmt.Print("Primeiro ")
+	fmt.Print("Programa!")
+}
